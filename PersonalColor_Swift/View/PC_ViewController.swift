88084689245af24486a6a2dc4b067e1f30e2c9f5
강대규 <rkdeorb0124@naver.com>
2023-09-21 15:30:34 +0900

    flask 서버 통신 구현

--- PersonalColor_Swift/View/PC_ViewController.swift
+++ PersonalColor_Swift/View/PC_ViewController.swift
@@ -15,17 +15,12 @@ class PC_ViewController: UIViewController {
     var itemProviders: [NSItemProvider] = []
     
     @IBOutlet var img_upload: UIImageView!
-    
-    
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
     @IBAction func btn_Slect_photo(_ sender: UIButton) {
         presentPicker()
@@ -57,7 +52,6 @@ class PC_ViewController: UIViewController {
     // 앨범선택사진 img에 띄우기
     func addPreviewImage(){
         
-        
         // 사진이 한 개이므로 first로 접근하여 itemProvider를 생성
         guard let itemProvider = itemProviders.first else { return }
         
@@ -79,28 +73,65 @@ class PC_ViewController: UIViewController {
         }
     } // func addPreviewImage() End-
     
-    
-    
 }
 
 
 
 // 앨범
-extension PC_ViewController: PHPickerViewControllerDelegate{
+extension PC_ViewController: PHPickerViewControllerDelegate {
+    // picker가 종료되면 동작 함
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        
+        // picker가 선택이 완료되면 화면 내리기
+        picker.dismiss(animated: true)
+        
+        // 만들어준 itemProviders에 Picker로 선택한 이미지정보를 전달
+        itemProviders = results.map(\.itemProvider)
+        
+        // 앨범에서 이미지 선택시 imgview에 보이기
+        if !itemProviders.isEmpty {
+            addPreviewImage()
+            
+            // 이미지가 골라지면 정면사진인지 확인
+            
+            if let image = img_upload.image {
+                if let imageData = image.jpegData(compressionQuality: 1.0) {
+                    // 이미지 데이터가 준비되었습니다.
+                    let serverURL = URL(string: "http://127.0.0.1:5000/rgb")! // Flask 서버 엔드포인트
+                    var request = URLRequest(url: serverURL)
+                    request.httpMethod = "POST" // POST 요청으로 수정
 
-        // picker가 종료되면 동작 함
-        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-            
-            // picker가 선택이 완료되면 화면 내리기
-            picker.dismiss(animated: true)
-            
-            // 만들어준 itemProviders에 Picker로 선택한 이미지정보를 전달
-            itemProviders = results.map(\.itemProvider)
-            
-            // 앨범에서 이미지 선택시 imgview에 보이기
-            if !itemProviders.isEmpty {
-                        addPreviewImage()
+                    // POST 요청 본문에 이미지 데이터 추가
+                    request.httpBody = imageData
+                    request.setValue("image/jpeg", forHTTPHeaderField: "Content-Type") // Content-Type 설정
+
+                    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                        if let error = error {
+                            print("Error: \(error)")
+                            return
+                        }
+
+                        if let data = data {
+                            // 서버에서 반환한 데이터를 처리합니다.
+                            do {
+                                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                                    // JSON 데이터를 파싱하여 원하는 작업을 수행합니다.
+                                    print("Received JSON: \(json)")
+                                }
+                            } catch {
+                                print("Error parsing JSON: \(error)")
+                            }
+                        }
                     }
+
+                    task.resume() // URLSession 태스크 실행
+                }
+            }
+
         }
-} // 앨범끗
+    }
+    
+
+}
+
 
