012c6b330c6659c7834c163ed34508c2deeb51eb
LJU <gosirock1@gmail.com>
2023-09-22 18:36:29 +0900

    보드게시판 새로구축

---
+++ PersonalColor_Swift/View/Detail_Board_ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  Detail_Board_ViewController.swift
+//  PersonalColor_Swift
+//
+//  Created by 이종욱 on 2023/09/22.
+//
+
+import UIKit
+
+class Detail_Board_ViewController: UIViewController {
+
+    
+    @IBOutlet var lbl: UILabel!
+    
+    
+    var name = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        lbl.text = name
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
